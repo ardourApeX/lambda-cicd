61cc2a2a95f6f35cf235701a6325d3133746d7c1
Joy <joygupta501@gmail.com>
2024-01-08 11:44:40 +0530

    Chore : Minor change

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 export const handler = async (event: APIGatewayProxyEvent) => {
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify('Hello from lambda!'),
+		body: JSON.stringify('Hello World from lambda!'),
 	};
 	return response;
 };
