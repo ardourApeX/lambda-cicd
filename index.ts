e225947731466bc880009d04909831de489200d4
Joy <joygupta501@gmail.com>
2024-01-09 22:38:56 +0530

    Feat : Testing dependency for lamdba

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import generateRandom from './generateRandom';
 export const handler = async (event: APIGatewayProxyEvent) => {
+	const randomNumber = generateRandom();
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify('Hello World from lambda!'),
+		body: JSON.stringify('Hello World from lambda! ' + randomNumber),
 	};
 	return response;
 };
